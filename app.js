c8fae15e0e44293b7784b9863a81ee7e483cd120
Gabriel Carlos Cavalcante <80012329+Gabrielttbr@users.noreply.github.com>
2022-05-23 15:11:04 -0300

    Arrumando a configuração cors da api

--- app.js
+++ app.js
@@ -17,11 +17,9 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extends: false}));
 app.use(bodyParser.json())
 app.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
     res.header("Access-Control-Allow-Origin", "*");
-	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 /*==============================================================================================*/
